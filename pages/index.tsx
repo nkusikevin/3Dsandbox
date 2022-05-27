d24195884a986ccd331acbe5befc7dcd2f5c9a6b
nkusikevin <nkusikvnhart@gmail.com>
2022-05-27 19:24:42 +0200

    adding floor

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import { Suspense } from "react";
 import { useGLTF } from "drei";
 import type { NextPage } from "next";
 import { Canvas } from "react-three-fiber";
+import Floor from "../components/Floor";
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<Canvas camera={{ postion: [0, 0, 120], fov: 70 }}>
-				<Suspense fallback={null}></Suspense>
+				{/* <Suspense fallback={null}></Suspense> */}
+				<Floor />
 			</Canvas>
 		</div>
 	);
