5c4d691e919962636657977792d87d4d381eebeb
nkusikevin <nkusikvnhart@gmail.com>
2022-05-28 19:31:56 +0200

    adding light and css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,20 @@ import { useGLTF } from "drei";
 import type { NextPage } from "next";
 import { Canvas } from "react-three-fiber";
 import Floor from "../components/Floor";
+import css from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<Canvas camera={{ postion: [0, 0, 120], fov: 70 }}>
+		<div className={css.scene}>
+			<Canvas
+				shadows={true}
+				className={css.canvas}
+				camera={{
+					position: [-6, 7, 7],
+				}}>
 				{/* <Suspense fallback={null}></Suspense> */}
-				<Floor />
+				<ambientLight color={"white"} intensity={0.3} />
+				<Floor position={[0, -1, 0]} />
 			</Canvas>
 		</div>
 	);
